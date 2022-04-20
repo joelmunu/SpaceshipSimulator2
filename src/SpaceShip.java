7745efbc7dd5be6c2f5f66d5dca61e7e9cd0ed63
joelmunu <joel.munu@gmail.com>
2022-04-20 19:56:28 +0100

    Feature: Añadido el método speedUp en la clase SpaceShip, y añadido el atributo EjeSeleccionado para permitir al usuario seleccionar el eje en el que se moverá la nave

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,18 +1,39 @@
 public class SpaceShip {
     private String nombre;
     private String matriculaGalatica;
-    private float aceleracion;
-    private float velocidadX = 0;
-    private float velocidadY = 0;
+    private int aceleracion;
+    private int velocidadX = 0;
+    private int velocidadY = 0;
     private int coordenadaX = 0;
-    private int getCoordenadaY = 0;
+    private int coordenadaY = 0;
     private int direccionX = 0;
     private int direccionY = 0;
+    private int ejeSeleccionado;
 
-    public SpaceShip(String nombre, String matriculaGalatica, float aceleracion) {
+    public SpaceShip(String nombre, String matriculaGalatica, int aceleracion, int ejeSeleccionado) {
         this.nombre = nombre;
         this.matriculaGalatica = matriculaGalatica;
         this.aceleracion = aceleracion;
+        this.ejeSeleccionado = ejeSeleccionado;
+    }
+
+    // Método speedUp()
+    public void speedUp() {
+        if (getEjeSeleccionado() == 0) {
+            velocidadX += aceleracion;
+            coordenadaX += velocidadX;
+            System.out.println("Acelerando en el eje X");
+            System.out.println("Velocidad X actual: " + velocidadX + " km/h");
+            System.out.println("Coordenadas actuales: X: " + coordenadaX + " , Y: " + coordenadaY);
+        } else if (getEjeSeleccionado() == 1) {
+            velocidadY += aceleracion;
+            coordenadaY += aceleracion;
+            System.out.println("Acelerando en el eje Y");
+            System.out.println("Velocidad Y actual: " + velocidadY + " km/h");
+            System.out.println("Coordenadas actuales: X: " + coordenadaX + " , Y: " + coordenadaY);
+        } else {
+            System.out.println("ERROR: debes introducir X o Y");
+        }
     }
 
     public String getNombre() {
@@ -31,27 +52,27 @@ public class SpaceShip {
         this.matriculaGalatica = matriculaGalatica;
     }
 
-    public float getAceleracion() {
+    public int getAceleracion() {
         return aceleracion;
     }
 
-    public void setAceleracion(float aceleracion) {
+    public void setAceleracion(int aceleracion) {
         this.aceleracion = aceleracion;
     }
 
-    public float getVelocidadX() {
+    public int getVelocidadX() {
         return velocidadX;
     }
 
-    public void setVelocidadX(float velocidadX) {
+    public void setVelocidadX(int velocidadX) {
         this.velocidadX = velocidadX;
     }
 
-    public float getVelocidadY() {
+    public int getVelocidadY() {
         return velocidadY;
     }
 
-    public void setVelocidadY(float velocidadY) {
+    public void setVelocidadY(int velocidadY) {
         this.velocidadY = velocidadY;
     }
 
@@ -63,12 +84,12 @@ public class SpaceShip {
         this.coordenadaX = coordenadaX;
     }
 
-    public int getGetCoordenadaY() {
-        return getCoordenadaY;
+    public int getCoordenadaY() {
+        return coordenadaY;
     }
 
-    public void setGetCoordenadaY(int getCoordenadaY) {
-        this.getCoordenadaY = getCoordenadaY;
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
     }
 
     public int getDireccionX() {
@@ -85,5 +106,13 @@ public class SpaceShip {
 
     public void setDireccionY(int direccionY) {
         this.direccionY = direccionY;
+    }
+
+    public int getEjeSeleccionado() {
+        return ejeSeleccionado;
+    }
+
+    public void setEjeSeleccionado(int ejeSeleccionado) {
+        this.ejeSeleccionado = ejeSeleccionado;
     }
 }
